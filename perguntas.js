ec182dc4134f482cf42efc6c38e05f96a6a4492a
Isac346 <isac.valezi@escola.pr.gov.br>
2025-06-18 01:48:51 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'McDonald\'s',
+    'O que é o McDonald\'s?',
+    'O McDonald\'s é uma rede de fast-food fundada nos Estados Unidos, conhecida mundialmente por seus hambúrgueres.'
+)
+
+criaCartao(
+    'McDonald\'s',
+    'Qual é o sanduíche mais famoso do McDonald\'s?',
+    'O sanduíche mais famoso do McDonald\'s é o Big Mac.'
+)
+
+criaCartao(
+    'McDonald\'s',
+    'O que é o McLanche Feliz?',
+    'O McLanche Feliz é um combo infantil do McDonald\'s que acompanha brinquedo temático.'
+)
+
+criaCartao(
+    'McDonald\'s',
+    'Onde surgiu o McDonald\'s?',
+    'O McDonald\'s surgiu na Califórnia, nos Estados Unidos, em 1940.'
+)
